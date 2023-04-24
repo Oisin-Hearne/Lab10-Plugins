cf4183b1e55667a43ca64b678966ea40a1b7891e
Oisin Hearne <oisinhearne@gmail.com>
2023-04-24 09:52:04 +0100

    Implemented plugins (Browser & GPS)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
+  lon:number=0;
+  lat:number=0;
   constructor() {}
-  
+
+  //Sets the value of lon and lat equal to the current longitute and latitude of the user device.
+  async getGPS() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.lon = coordinates.coords.longitude; //If this seems a bit confusing, check the documentation
+    this.lat = coordinates.coords.latitude; //Or try seeing what autofill gives you when you hit tab
+  }
+
+  //Opens a Browser Window to the designated URL.
+  async openBrowser() {
+    await Browser.open({url: 'http://capacitorjs.com/'});
+  }
 }
+
